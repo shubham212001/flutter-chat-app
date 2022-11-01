9d3516daeed23e31cd3d664ebce84b9d0a45bb6a
Shubham Sharma <shubham.sharma200121@gmail.com>
2022-11-01 15:29:42 +0530

    Welcome screen UI

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:digichat/screens/Welcome.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home:WelcomeScreen()));
+}
---
+++ lib/screens/Welcome.dart
@@ -0,0 +1,59 @@
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3a57e8),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(35, 16, 32, 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 10, 70),
+              child: Text(
+                "Wecome To DigiChat",
+                textAlign: TextAlign.start,
+                overflow: TextOverflow.clip,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 32,
+                  color: Color(0xffffffff),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 300, 0, 0),
+              child: MaterialButton(
+                onPressed: () {},
+                color: Color(0xffffffff),
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24.0),
+                ),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  "Next",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+                textColor: Color(0xff3a57e8),
+                height: 50,
+                minWidth: 500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
